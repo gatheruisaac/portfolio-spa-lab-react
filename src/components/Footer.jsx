f28dbfce44cf44448742533c677c89a2b42b9057
isaac gatheru <igatheru7@gmail.com>
2026-02-05 15:45:26 +0300

    feat: add footer component & styling kijiji

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2026 My Portfolio. All rigths reserved.</p>
+      <div className="social-links">
+        <a href="https://twitter.com">Twitter</a>
+        <a href="https://linkedin.com">LinkedIn</a>
+        <a href="https://github.com">GitHub</a>
+      </div>
+    </footer>
+  );
+}
